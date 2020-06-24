722ff2fb8ba09c3188e0b52e44f7fc62a734a166
adillson <ad.medronha@gmail.com>
2020-06-23 23:35:42 -0300

    avaliation implemented!

---
+++ src/main/java/com/project/app/entities/Avaliacao.java
@@ -0,0 +1,39 @@
+package com.project.app.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Avaliacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    Anuncio anuncio;
+
+    @ManyToOne
+    User user;
+
+    String comentario;
+    int pontuacao;
+
+    public Avaliacao(Anuncio anuncio, User user, String comentario, int pontuacao) {
+        this.pontuacao = pontuacao;
+        this.anuncio = anuncio;
+        this.user = user;
+        this.comentario = comentario;
+    }
+
+
+}
